4bbd632c724305bf50d38bf86b5a311398d7b084
ProXor652 <apmetalnikov@gmail.com>
2022-09-11 17:50:30 +0700

    Task 1,2

--- Task6.cs
+++ Task6.cs
@@ -1,4 +1,4 @@
-﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+﻿/* //Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
 
 Console.WriteLine("Enter numbers separated by spaces: ");
 int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
@@ -11,6 +11,20 @@ for(int i = 0; i < array.Length; ++i)
     count++;
     }
  }
-  Console.WriteLine("Number of elements > 0 = " + count);
+  Console.WriteLine("Number of elements > 0 = " + count); */
 
 //Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Enter number b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter number b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter number k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter number k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Intersection point: ({x};{y})");
